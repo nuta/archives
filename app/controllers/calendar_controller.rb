c815a40097fe2180c9b971564945135d13e733e0
Seiya Nuta <nuta@seiya.me>
2015-09-26 21:55:48 +0900

    return 501 for now

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -6,22 +6,25 @@ class CalendarController < ApplicationController
   end
 
   def get
-    render :text => 'not implemented'
+    render :text => 'not implemented', :status => :not_implemented
   end
 
   def put
-    render :text => 'not implemented'
+    render :text => 'not implemented', :status => :not_implemented
   end
 
   def delete
-    render :text => 'not implemented'
+    render :text => 'not implemented', :status => :not_implemented
   end
 
   def mkcalendar
-    render :text => 'not implemented'
+    render :text => 'not implemented', :status => :not_implemented
   end
 
   def propfind
-    render :text => 'not implemented'
+    res = ""
+    render :text => res,
+           :content_type => 'application/xml',
+           :status => :multi_status
   end
 end
