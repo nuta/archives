200af871ef7ab52d677db564a508e0471ce9cbb7
Seiya Nuta <nuta@seiya.me>
2015-11-12 13:08:06 +0900

    route: return 404 for an invalid request path

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   methods.each do |method|
     match '/calendar:uri', uri: /.*/, :to => 'calendar#' + method, :via => method.to_s
   end
+
+  # other requests: return 404 Bad Request
+  match '*otherwise', to: proc { [400, {}, ['']] }, via: methods
 end
