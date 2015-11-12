9cf78c246f64431ceae6068188dd2992fd1e661e
Seiya Nuta <nuta@seiya.me>
2015-11-12 13:07:30 +0900

    route: remove :user from /calendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  ['options', 'get', 'put', 'delete', 'mkcalendar',
-   'propfind', 'proppatch', 'report'].each do |method|
-    match '/:user/calendar:uri', uri: /.*/, :to => 'calendar#' + method, :via => method.to_s
+  methods = %w(options get put delete mkcalendar propfind proppatch report)
+
+  methods.each do |method|
+    match '/calendar:uri', uri: /.*/, :to => 'calendar#' + method, :via => method.to_s
   end
 end
