6a7458d04843cafcdbad1cea325d9f23a3ef6734
Seiya Nuta <nuta@seiya.me>
2015-12-18 23:13:53 +0900

    update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,12 +1,10 @@
-app_dir = File.expand_path("../..", __FILE__)
-unicorn_dir = "#{app_dir}/unicorn"
-working_directory app_dir
+working_directory File.expand_path("../..", __FILE__)
 
 worker_processes 2
 preload_app true
 timeout 30
 
-listen "#{unicorn_dir}/unicorn.sock", :backlog => 64
-pid "#{unicorn_dir}/unicorn.pid"
+listen "/var/run/unicorn.sock", :backlog => 64
+pid "/var/run/unicorn.pid"
 stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
