e2aa6e7b16857a257743c843d448749de8287c81
Seiya Nuta <nuta@seiya.me>
2015-12-27 14:56:26 +0900

    specify the calendar in Schedule.in_time_range()

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,7 @@
 class Schedule < ActiveRecord::Base
   belongs_to :calendar
 
-  def Schedule.in_time_range(range_start, range_end)
+  def Schedule.in_time_range(calendar, range_start, range_end)
     sql = ''
     args = []
 
@@ -18,6 +18,6 @@ class Schedule < ActiveRecord::Base
 	args << range_end
     end
 
-    self.where(sql, *args)
+    self.where(calendar: Calendar.find_by(calendar)).where(sql, *args)
   end
 end
