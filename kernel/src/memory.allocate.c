5d53d888aec5a269a7c5572cac262f0013f9c3e3
Seiya Nuta <nuta@seiya.me>
2016-04-13 11:11:52 +0900

    do cast to suppress a compiler warning

--- kernel/src/memory.allocate.c
+++ kernel/src/memory.allocate.c
@@ -14,7 +14,7 @@ void kernel_memory_allocate(channel_t __ch, size_t size, uint32_t flags) {
      result_t r;
      uintptr_t addr;
 
-     addr = kernel_allocate_memory(size, flags);
+     addr = (uintptr_t) kernel_allocate_memory(size, flags);
      r = (addr)? OK : E_NOSPACE;
 
      send_memory_allocate_reply(__ch, r, addr);
