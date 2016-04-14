07172a804ec60df5b93deac480dfbc16ca906ee7
Seiya Nuta <nuta@seiya.me>
2016-04-14 14:50:43 +0900

    travis: create database.yml before db:migrate

---
+++ travis/script.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+exitcode=0
+
+cd test
+
+RAILS_ENV=test COVERALLS=true bundle exec rails server 3>&1 > log &
+PID=$!
+
+sleep 5
+
+bundle exec rake test:run
+exitcode=$?
+
+kill -2 $PID
+wait $PID
+
+echo "---------------------------------------"
+cat log
+
+exit $exitcode
