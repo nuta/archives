964416584ba68c1c2671c9ed9562e4799c403326
Seiya Nuta <nuta@seiya.me>
2016-04-14 14:45:19 +0900

    test with postgresql and mysql (closes #16)

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,30 @@ exitcode=0
 
 cd test
 
-COVERALLS=true bundle exec rails server 3>&1 > log &
+cat << EOF > config/database.yml
+sqlite: &sqlite
+  adapter: sqlite3
+  database: db/test.sqlite3
+
+mysql: &mysql
+  adapter: mysql
+  username: root
+
+postgresql: &postgresql
+  adapter: postgresql
+  username: postgres
+
+test:
+  pool: 5
+  timeout: 5000
+  host: localhost
+  password:
+  database: busybook_test
+  <<: *<%= ENV['DB'] %>
+
+EOF
+
+RAILS_ENV=test COVERALLS=true bundle exec rails server 3>&1 > log &
 PID=$!
 
 sleep 5
