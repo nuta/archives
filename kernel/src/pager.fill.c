e57e0917a6d2170b963e9e208dd2793e7398be01
Seiya Nuta <nuta@seiya.me>
2016-04-19 10:21:50 +0900

    kernel: use sendas()

--- kernel/src/pager.fill.c
+++ kernel/src/pager.fill.c
@@ -7,6 +7,8 @@
 /** handles pager.fill */
 void kernel_pager_fill(channel_t __ch, ident_t id, offset_t offset, size_t size) {
 
-    send_pager_fill_reply(__ch, 
-        OK, MOVE(kernel_allocate_memory(size, MEMORY_ALLOC_NORMAL)), size);
+    sendas_pager_fill_reply(__ch, 
+        OK, PAYLOAD_INLINE,
+        kernel_allocate_memory(size, MEMORY_ALLOC_NORMAL), PAYLOAD_MOVE_OOL,
+	size, PAYLOAD_INLINE);
 }
