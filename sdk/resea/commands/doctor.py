2e36d60a8d0ec289ac685ef2dfed9d960c737c59
Seiya Nuta <nuta@seiya.me>
2016-05-12 07:59:04 +0900

    fix doctor

--- sdk/resea/commands/doctor.py
+++ sdk/resea/commands/doctor.py
@@ -1,3 +1,4 @@
+import argparse
 import datetime
 import os
 import sys
@@ -29,7 +30,7 @@ INDEX_HTML = """\
 </html>
 """
 
-def main(argv_):
+def doctor():
     resea.commands.clean.main([])
     tmp_dir = os.path.join('tmp', 'doctor')
     shutil.rmtree(tmp_dir, ignore_errors=True)
@@ -46,20 +47,23 @@ def main(argv_):
     except FileNotFoundError:
         sys.exit("'package.yml' not found")
 
-    config, _ = load_packages(yml['depends'])
-    lang = yml.get("lang")
-    if lang is None:
-        error("lang is not speicified in package.yml")
+    if yml['category'] in ['application', 'library']:
+        config, _ = load_packages([yml['name']] + yml['depends'])
+        lang = yml.get("lang")
+        if lang is None:
+            error("lang is not speicified in package.yml")
 
-    # run lang's doctor
-    lang_html_path = os.path.join(tmp_dir, 'lang.html')
-    subprocess.run([config['LANG'][lang]['doctor'], lang_html_path,
-        tmp_dir], check=True)
+        # run lang's doctor
+        lang_html_path = os.path.join(tmp_dir, 'lang.html')
+        subprocess.run([config['LANG'][lang]['doctor'], lang_html_path,
+            tmp_dir], check=True)
 
-    # generate index.html
-    progress('Generating index.html')
-    with open(lang_html_path) as f:
-        lang_html = f.read()
+        # generate index.html
+        progress('Generating index.html')
+        with open(lang_html_path) as f:
+            lang_html = f.read()
+    else:
+        lang_html = ''
 
     index_html_path = os.path.join(tmp_dir, 'index.html')
     with open(index_html_path, 'w') as f:
@@ -68,7 +72,21 @@ def main(argv_):
                     'created_at': str(datetime.datetime.now())
                 }))
 
-    progress('Opening the results')
-    webbrowser.open_new_tab('file://' + os.path.abspath(index_html_path))
+    return index_html_path
 
-    plan('Done all diagnosis. Check out the details in the web browser.')
+
+def main(argv_):
+    parser = argparse.ArgumentParser(prog='resea doctor',
+                                     description='diagnose the package')
+    parser.add_argument('--open-browser', action='store_true',
+        help='open results in a web browser')
+    args = parser.parse_args(argv_)
+
+    index_html_path = doctor()
+
+    if args.open_browser:
+        progress('Opening the results')
+        webbrowser.open_new_tab('file://' + os.path.abspath(index_html_path))
+
+    plan('Done all diagnosis')
+
