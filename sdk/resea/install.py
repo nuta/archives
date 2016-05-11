08bdd946b28cf52522defa8068cd6a18a742f481
Seiya Nuta <nuta@seiya.me>
2016-05-11 12:10:44 +0900

    sdk: fix ubuntu_install

--- sdk/resea/install.py
+++ sdk/resea/install.py
@@ -3,58 +3,27 @@ import subprocess
 from resea.helpers import plan, error
 
 
-def osx_update():
-    try:
-        subprocess.run(['apt-get', 'update', '-y'], check=True)
-    except subprocess.CalledProcessError:
-        error('failed to update the list of packages'.format(args))
+def osx_install(requirements):
+    def update():
+        try:
+            subprocess.run(['apt-get', 'update', '-y'], check=True)
+        except subprocess.CalledProcessError:
+            error('failed to update the list of packages'.format(args))
+    
+    def osx_is_installed(package):
+        p = subprocess.run(['brew', 'list', package],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return p.returncode == 0
 
 
-def osx_is_installed(package):
-    p = subprocess.run(['brew', 'list', package],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return p.returncode == 0
-
-
-def osx_install(args):
-    try:
-        subprocess.run(['brew', 'install'] + args, check=True)
-    except subprocess.CalledProcessError:
-        error('failed to install {}'.format(args))
-
-
-def ubuntu_update():
-    try:
-        subprocess.run(['brew', 'update'], check=True)
-    except subprocess.CalledProcessError:
-        error('failed to update the list of packages'.format(args))
-
-
-def ubuntu_is_installed(package):
-    p = subprocess.run(['dpkg', '-s', package],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return p.returncode == 0
-
-
-def ubuntu_install(args):
-    try:
-        subprocess.run(['apt-get', 'install', '-y'] + args, check=True)
-    except subprocess.CalledProcessError:
-        error('failed to install {}'.format(args))
-
-
-def install_os_requirements(os_requirements):
-    os, update, is_installed, install = {
-        'Darwin': ('osx', osx_update, osx_is_installed, osx_install),
-        'Linux':  ('ubuntu', ubuntu_update, ubuntu_is_installed, ubuntu_install),
-    }.get(platform.system(), ('', None, None, None))
-
-    if install is None:
-        return
-
-    requirements = os_requirements.get(os, {})
+    def osx_install(args):
+        try:
+            subprocess.run(['brew', 'install'] + args, check=True)
+        except subprocess.CalledProcessError:
+            error('failed to install {}'.format(args))
+        
     missing = [] 
-    for package in requirements.get('homebrew', {}).get('packages', []):
+    for package in requirements.get('apt', {}).get('packages', []):
         args = package.split(' ')
         package = list(filter(lambda x: not x.startswith('-'), args))[0]
         if not is_installed(package):
@@ -65,4 +34,50 @@ def install_os_requirements(os_requirements):
         update()
         for package in missing:
             install(package)
+
+    
+ 
+def ubuntu_install(requirements):
+    def update():
+        try:
+            subprocess.run(['brew', 'update'], check=True)
+        except subprocess.CalledProcessError:
+            error('failed to update the list of packages'.format(args))
+    
+    
+    def is_installed(package):
+        p = subprocess.run(['dpkg', '-s', package],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return p.returncode == 0
+
+    def install(args):
+        try:
+            subprocess.run(['apt-get', 'install', '-y'] + args, check=True)
+        except subprocess.CalledProcessError:
+            error('failed to install {}'.format(args))
+        
+    missing = [] 
+    for package in requirements.get('apt', {}).get('packages', []):
+        args = package.split(' ')
+        package = list(filter(lambda x: not x.startswith('-'), args))[0]
+        if not is_installed(package):
+            missing.append(args)
+
+    if missing != []:
+        plan('Install requirements')
+        update()
+        for package in missing:
+            install(package)
+
+
+def install_os_requirements(os_requirements):
+    os, install = {
+        'Darwin': ('osx', osx_install),
+        'Linux':  ('ubuntu', ubuntu_install),
+    }.get(platform.system(), ('', None))
+
+    if install is None:
+        return
+
+    install(os_requirements.get(os, {}))
 
