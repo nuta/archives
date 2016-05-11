a24aca190f5792e3823ed53c955bf707529174bc
Seiya Nuta <nuta@seiya.me>
2016-05-11 11:26:40 +0900

    sdk: support osx_requirements with install options

--- sdk/resea/install.py
+++ sdk/resea/install.py
@@ -4,21 +4,23 @@ from resea.helpers import plan, error
 
 
 def osx_install(requirements):
-    def is_installed(package):
+    def is_installed(args):
+        package = list(filter(lambda x: not x.startswith('-'), args))[0]
         p = subprocess.run(['brew', 'list', package],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return p.returncode == 0
 
-    def install_package(package):
+    def install_package(args):
         try:
-            subprocess.run(['brew', 'install', package], check=True)
+            subprocess.run(['brew', 'install'] + args, check=True)
         except subprocess.CalledProcessError:
-            error('failed to install {}'.format(package))
+            error('failed to install {}'.format(args))
 
     missing = [] 
     for package in requirements.get('homebrew', {}).get('packages', []):
-        if not is_installed(package):
-            missing.append(package)
+        args = package.split(' ')
+        if not is_installed(args):
+            missing.append(args)
 
     if missing != []:
         plan("Install requirements")
