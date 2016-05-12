f7cd297c0f7d27c47f5ebad671a5d2c2b8650894
Seiya Nuta <nuta@seiya.me>
2016-05-12 09:32:19 +0900

    sdk: update package.yml validator

--- sdk/resea/validators.py
+++ sdk/resea/validators.py
@@ -7,18 +7,18 @@ class ValidationError(Exception):
 
 def validate_package_yml(d):
     try:
-        desc = 'it should be a dict'
+        desc = 'it must be a dict'
         assert isinstance(d, dict)
 
         for x in ['name', 'category', 'license', 'homepage']:
-            desc = "'{}' is undefined".format(x)
+            desc = "'{}' must be defined".format(x)
             assert x in d
 
-        desc = 'name should matches /^[a-z][a-z0-9_]*$/'
+        desc = 'name must matches /^[a-z][a-z0-9_]*$/'
         assert re.match('^[a-z][a-z0-9_]*$', d['name'])
 
         for x in ['description', 'author', 'email', 'info', 'warning']:
-            desc = '{} should be a string'.format(x)
+            desc = '{} must be a string'.format(x)
             if x not in d or d[x] is None:
                 d[x] = ""
             assert isinstance(d[x], str)
@@ -27,61 +27,66 @@ def validate_package_yml(d):
                   'uses', 'conflicts']:
             if x not in d or d[x] is None:
                 d[x] = []
-            desc = '{} should be a list'.format(x)
+            desc = '{} must be a list'.format(x)
             assert isinstance(d[x], list)
 
         for x in ['os_requirements', 'interface', 'types']:
             if x not in d or d[x] is None:
                 d[x] = {}
-            desc = '{} should be a dict'.format(x)
+            desc = '{} must be a dict'.format(x)
             assert isinstance(d[x], dict)
 
         # category
         cats = ['application', 'library', 'interface', 'group', 'misc']
-        desc = 'category should be one of the following: {}'.format(
+        desc = 'category must be one of the following: {}'.format(
             ', '.join(cats))
         assert d['category'] in cats
 
         # interface
         for name, i in d['interface'].items():
-            desc = '.{}: message type name should matches ' \
+            for x in ['type', 'id', 'payloads']:
+                desc = ".{}: '{}' must be defined".format(name, x)
+                assert x in i
+
+            msg_types = ['request', 'reply', 'push']
+            desc = '.{}: message type must be one of the following: {}'.format(
+                       name, ', '.join(msg_types))
+            assert i['type'] in msg_types
+
+            desc = '.{}: message name must matches ' \
                    '/^[a-z][a-z0-9_]*$/'.format(name)
             assert re.match('^[a-z][a-z0-9_]*$', name)
 
-            desc = ".{}: message type should have 'payloads'" \
-                   "and 'id'".format(name)
-            assert 'payloads' in i and 'id' in i
-
-            desc = '.{}: message type ID should an int and it should be' \
+            desc = '.{}: message ID must an int and it must be' \
                    'larger than 0'.format(name)
             assert isinstance(i['id'], int) and i['id'] > 0
 
-            desc = ".{}: 'payloads' should be a list or None".format(name)
+            desc = ".{}: 'payloads' must be a list or None".format(name)
             assert isinstance(i['payloads'], list) or i['payloads'] is None
             if i['payloads'] is not None:
                for x in i['payloads']:
-                   desc = ".{}: interface should have" \
+                   desc = ".{}: interface must have" \
                           "'name' and 'type'".format(name)
                    assert 'name' in x and 'type' in x
 
-                   desc = '.{}({}): payload name should matches ' \
+                   desc = '.{}({}): payload name must matches ' \
                           '/^[a-z][a-z0-9_]*$/'.format(
                               name, x['name'])
                    assert re.match('^[a-z][a-z0-9_]*$', x['name'])
 
         # types
         for name, t in d['types'].items():
-            desc = '.{}: type name should matches ' \
+            desc = '.{}: type name must matches ' \
                    '/^[a-z][a-z0-9_]*$/'.format(name)
             assert re.match('^[a-z][a-z0-9_]*$', name)
 
-            desc = "{}: type should have 'type'".format(name)
+            desc = "{}: type must have 'type'".format(name)
             assert 'type' in t
 
             if t['type'] == 'const':
-                desc = "{}: type should have 'size'".format(name)
+                desc = "{}: type must have 'size'".format(name)
                 assert 'size' in t
-                desc = "{}: const type should have 'consts'".format(name)
+                desc = "{}: const type must have 'consts'".format(name)
                 assert 'consts' in t
             else:
                 desc = "{}: unknown type: '{}'".format(name, t['type'])
