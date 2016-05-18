9ba448e42c89fb09d08b799cdeffe0080ac376ab
Seiya Nuta <nuta@seiya.me>
2016-05-18 11:46:19 +0900

    sdk: fix run command

--- sdk/resea/commands/run.py
+++ sdk/resea/commands/run.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import sys
 from resea.run import run_emulator
-from resea.helpers import info, notice, error, plan, progress
+from resea.helpers import info, notice, error, plan, progress, dict_to_strdict
 from resea.commands.build import build, add_build_arguments
 
 
@@ -15,22 +15,22 @@ Usage: resea run
 
 
 def run(args):
-    config = build(args)
+    config = build(args, {})
 
     env = os.environ.copy()
-    env.update(config)
+    env.update(dict_to_strdict(config))
 
     plan('Generating a disk image')
     cmd = [config['HAL_GENIMAGE'], config['EXECUTABLE_PATH'],
            config['BUILD_DIR'] + '/disk.img']
     progress(' '.join(cmd))
 
-    subprocess.Popen(cmd, env=env).wait()
+    subprocess.Popen(' '.join(cmd), shell=True).wait()
 
     plan('Launching an emulator')
     cmd = [config['HAL_RUN'], config['BUILD_DIR'] + '/disk.img']
     progress(' '.join(cmd))
-    run_emulator(cmd, env=env)
+    run_emulator(cmd, env)
 
 def main(args):
     parser = argparse.ArgumentParser(prog='resea run',
