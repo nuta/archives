fc125c32711d871d669412d2faa3f8b00582365e
Seiya Nuta <nuta@seiya.me>
2016-05-18 09:30:02 +0900

    sdk: osx: support Homebrew taps

--- sdk/resea/install.py
+++ sdk/resea/install.py
@@ -10,20 +10,35 @@ def osx_install(requirements):
         except subprocess.CalledProcessError:
             error('failed to update the list of packages'.format(args))
     
-    def osx_is_installed(package):
+    def is_tapped(tap):
+        taps = subprocess.check_output(['brew', 'tap']) \
+               .decode('utf-8') \
+               .split('\n')
+        return tap in taps
+    
+    def is_installed(package):
         p = subprocess.run(['brew', 'list', package],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return p.returncode == 0
 
-
-    def osx_install(args):
+    def brew_install(args):
         try:
             subprocess.run(['brew', 'install'] + args, check=True)
         except subprocess.CalledProcessError:
             error('failed to install {}'.format(args))
-        
+
+    def brew_tap(tap):
+        try:
+            subprocess.run(['brew', 'tap', tap], check=True)
+        except subprocess.CalledProcessError:
+            error('failed to tap {}'.format(args))
+ 
+    for tap in requirements.get('homebrew', {}).get('taps', []):
+        if not is_tapped(tap):
+            brew_tap(tap)
+
     missing = [] 
-    for package in requirements.get('apt', {}).get('packages', []):
+    for package in requirements.get('homebrew', {}).get('packages', []):
         args = package.split(' ')
         package = list(filter(lambda x: not x.startswith('-'), args))[0]
         if not is_installed(package):
@@ -33,7 +48,7 @@ def osx_install(requirements):
         plan('Install requirements')
         update()
         for package in missing:
-            install(package)
+            brew_install(package)
 
     
  
