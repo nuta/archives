beaa055466eb22b8461c83c4cb065ba7c5c48e1b
Seiya Nuta <nuta@seiya.me>
2016-06-06 08:15:20 +0900

    virtio: launch virtio_net driver first

--- device/virtio/src/startup.cpp
+++ device/virtio/src/startup.cpp
@@ -13,8 +13,16 @@ channel_t virtio_server;
 
 void virtio_startup(void) {
 
+    INFO("starting");
+
     virtio_server = create_channel();
-    virtio_blk_init();
+
+    // virito_server will be regitered in init functions below
+    set_channel_handler(virtio_server, &virtio_handler);
+
     virtio_net_init();
+    virtio_blk_init();
+
+    INFO("ready");
     serve_channel(virtio_server, &virtio_handler);
 }
