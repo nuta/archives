1aab66ceb07276f288bd580d38692b85d6c03fed
Seiya Nuta <nuta@seiya.me>
2016-06-06 12:17:05 +0900

    deprecate genimage

--- sdk/resea/commands/run.py
+++ sdk/resea/commands/run.py
@@ -17,15 +17,8 @@ Usage: resea run
 def run(args):
     build(args)
 
-    plan('Generating a disk image')
-    cmd = [get_var('HAL_GENIMAGE'), get_var('EXECUTABLE_PATH'),
-           get_var('BUILD_DIR') + '/disk.img']
-    progress(' '.join(cmd))
-
-    subprocess.Popen(' '.join(cmd), shell=True).wait()
-
     plan('Launching an emulator')
-    cmd = [get_var('HAL_RUN'), get_var('BUILD_DIR') + '/disk.img']
+    cmd = [get_var('HAL_RUN'), get_var('EXECUTABLE_PATH')]
     progress(' '.join(cmd))
     run_emulator(cmd)
 
