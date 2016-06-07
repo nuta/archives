00a47bf956fe7ebe440f249e63f776723b53a051
Seiya Nuta <nuta@seiya.me>
2016-06-07 10:20:59 +0900

    arm: remove dead code

--- hal/arm/src/_hal.h
+++ hal/arm/src/_hal.h
@@ -3,17 +3,7 @@
 
 #include <resea.h>
 
-/*
- *  spinlock
- */
-#define SPIN_UNLOCKED 0
-#define SPIN_LOCKED   1
-typedef uint8_t spinlock_t;
-typedef uintmax_t irqstatus_t;
-typedef uintmax_t cpuid_t;
-
-/* page size is 4KiB */
-#define PAGE_SIZE      4096
+#define PAGE_SIZE 4096
 
 struct hal_vm_space{
 };
