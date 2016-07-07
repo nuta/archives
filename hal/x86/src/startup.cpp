db0c4f878abd5d9a3c4b13999fad8e3d8d59c8f7
Seiya Nuta <nuta@seiya.me>
2016-07-07 12:03:45 +0900

    x86: refactor

--- hal/x86/src/startup.cpp
+++ hal/x86/src/startup.cpp
@@ -2,20 +2,14 @@
 #include "x86.h"
 
 
-static channel_t ch;
-static struct hal_pmmap bsp_pmmap[16];
-static struct hal_vmmap bsp_vmmap[16];
+namespace {
+    struct hal_pmmap bsp_pmmap[16];
+    struct hal_vmmap bsp_vmmap[16];
+}
+
 void start_apps(void);
 extern "C" void kernel_startup(void);
 void x86_init_localapic_timer(void);
-
-
-void hal_startup(void) {
-
-    /* TSS */
-    x86_init_tss(&CPUVAR->tss);
-    x86_asm_ltr(GDT_TSS_SEG);
-}
 
 
 struct hal_pmmap *hal_get_pmmaps(void) {
@@ -30,23 +24,19 @@ struct  hal_vmmap *hal_get_vmmaps(void) {
 }
 
 
-/*
- *  This function MUST be called only from BSP, only once.
- *
- */
+//
+//  Initializes GDT, IDT, TSS, and other x86-related stuffs and
+//  calls the kernek. This function MUST be called only from BSP, only once.
+//
 void x86_init(void *binfo) {
     uint32_t *p;
     uint32_t type, size;
     uint64_t q;
 
     x86_init_print();
-    x86_disable_pic(); /* disable PIC; use IO APIC instead */
+    x86_disable_pic(); // disable PIC; use IO APIC instead
     x86_init_localapic();
 
-    /*
-     *  construct a physical memory map
-     *
-     */
     // look for memory map passed by GRUB2
     p = (uint32_t *) ((paddr_t) binfo + 8);
     for (;;) {
@@ -70,19 +60,20 @@ void x86_init(void *binfo) {
         PANIC("memory map from GRUB2 not found");
     }
 
-    // convert to hal_pmmap
-    uint32_t i,j;
+    // convert the GRUB2 memory map to hal_pmmap
+    uint32_t i, j;
     uint32_t num = (size - 16) / sizeof(struct grub_mmap_entry);
     struct grub_mmap_entry *m = (struct grub_mmap_entry *) ((paddr_t) p + 16);
 
     for (i=0, j=0; i < 15 && j < num; j++) {
 
-        // is available RAM and the address does not starts with 0?
         if ((m->type & 0xffffffff) == 1 && m->base > 0) {
             DEBUG("found a available RAM space: addr=%p, size=%dMiB",
                   m->base, m->length / 1024 / 1024);
 
             if (m->base < FREE_MEMORY_BASE_ADDR) {
+                // 0x0 - FREE_MEMORY_BASE_ADDR are reserved by kernel (the kernel
+                // image and page table exists)
                 bsp_pmmap[i].addr = FREE_MEMORY_BASE_ADDR;
                 bsp_pmmap[i].size = m->length - (FREE_MEMORY_BASE_ADDR - m->base);
             } else {
@@ -99,7 +90,7 @@ void x86_init(void *binfo) {
     bsp_pmmap[i].addr = 0;
     bsp_pmmap[i].size = 0;
 
-    /* virtual memory map */
+    // virtual memory map
     bsp_vmmap[0].type = VMMAP_USER;
     bsp_vmmap[0].addr = 0x0000000000000000;
     bsp_vmmap[0].size = KERNEL_BASE_ADDR - DYNAMIC_PAGETABLE_SIZE;
@@ -116,36 +107,37 @@ void x86_init(void *binfo) {
     bsp_vmmap[3].size = 0;
 
 
-    /* GDT */
+    // Initialize GDT
     x86_init_gdt((struct seg_desc *) &CPUVAR->gdt, &CPUVAR->tss);
     CPUVAR->gdtr.length  = GDT_LENGTH;
     CPUVAR->gdtr.address = (uint64_t) &CPUVAR->gdt;
     x86_asm_lgdt((uint64_t) &CPUVAR->gdtr);
 
-    /* IDT */
+    // Initialize IDT
     x86_init_idt((struct int_desc *) &CPUVAR->idt);
     CPUVAR->idtr.length  = IDT_LENGTH;
     CPUVAR->idtr.address = (uint64_t) &CPUVAR->idt;
     x86_asm_lidt((uint64_t) &CPUVAR->idtr);
 
-    /* I/O APIC, etc. */
+    // Initialize I/O APIC, etc.
     x86_init_smp();
 
-    /* system call */
+    // Enable system call instructions
     x86_asm_init_syscall((void (*)) x86_asm_syscall_handler,
                          KERNEL_CODE64_SEG,
                          KERNEL_NULL_SEG);
 
-    /* virtual memory */
+    // Initialize virtual memory
     x86_init_vm();
 
-    /* start the kernel */
+    // start the kernel
     kernel_startup();
 
-    hal_startup();
+    x86_init_tss(&CPUVAR->tss);
+    x86_asm_ltr(GDT_TSS_SEG);
     x86_init_localapic_timer();
 
-    /* start applications */
+    // start applications
     start_apps();
 }
 
--- hal/x86/src/tss.cpp
+++ hal/x86/src/tss.cpp
@@ -10,6 +10,7 @@ void x86_init_tss (struct TSS *tss) {
     call_hal_callback(HAL_CALLBACK_ALLOCATE_MEMORY,
         0, INTERRUPT_STACK_SIZE, resea::interfaces::memory::ALLOC_WIRED,
         &v, &p);
+
     CPUVAR->intr_stack_addr = (uintptr_t) v + INTERRUPT_STACK_SIZE;
     tss->ist[INT_HANDLER_IST - 1] = CPUVAR->intr_stack_addr;
     tss->iomap  = sizeof(struct TSS);  // this disables I/O permission map
