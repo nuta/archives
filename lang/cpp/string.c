a4fc381af9412b040fdce2e78878cf1982132d83
Seiya Nuta <nuta@seiya.me>
2016-10-10 10:32:20 +0900

    add memcpy
    C++ requires this.

--- lang/cpp/string.c
+++ lang/cpp/string.c
@@ -1,6 +1,23 @@
 #include <string.h>
 
 
+// This is for C++ default constructors. Use memcpy_s instead.
+void *memcpy(void *dest, const void *src, size_t n) {
+
+    uint8_t *p = (uint8_t *) dest;
+    uint8_t *q = (uint8_t *) src;
+    while (n > 0) {
+        *p = *q;
+
+        p++;
+        q++;
+        n--;
+    }
+
+    return dest;
+}
+
+
 result_t memcpy_s(void *dest, size_t dest_size, const void *src, size_t size) {
 
     if (dest_size < size || dest == NULL || src == NULL) {
