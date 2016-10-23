773c992373a05f6366b9f5b02f4df0e707fc6429
Seiya Nuta <nuta@seiya.me>
2016-10-23 10:09:53 +0900

    posix: override CXXFLAGS and CXX

--- arch/posix/arch.mk
+++ arch/posix/arch.mk
@@ -9,13 +9,15 @@ arch_objs += \
 
 LD := ld
 override CFLAGS += -DARCH_POSIX -ggdb3 -Os
+override CXXFLAGS += -DARCH_POSIX -ggdb3 -Os
 
 ifeq ($(shell uname -s), Linux)
 override CFLAGS += -pthread
 endif
 
 ifeq ($(shell uname -s), Darwin)
-CC := gcc-6
+CC  := gcc-6
+CXX := g++-6
 endif
 
 override CPPFLAGS += -I$(arch_dir)
