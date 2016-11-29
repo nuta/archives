582076f15d061546ef3e50b6c8c2673af6e1b133
Seiya Nuta <nuta@seiya.me>
2016-11-29 13:19:47 +0900

    fix

--- kernel/timer.c
+++ kernel/timer.c
@@ -1,10 +1,12 @@
+#include <resea/timer.h>
 #include "event.h"
+#include "kmalloc.h"
 #include "list.h"
 #include "timer.h"
 
 
 static struct timer *timers = NULL;
-static mutex_t timer_lock = MUTEX_INITIALIZER;
+static mutex_t timers_lock = MUTEX_INITIALIZER;
 
 void add_interval_timer(struct channel *ch, size_t interval_ms, uintmax_t arg) {
 
@@ -15,9 +17,9 @@ void add_interval_timer(struct channel *ch, size_t interval_ms, uintmax_t arg) {
     timer->current = interval_ms;
     timer->reset   = interval_ms;
 
-    mutex_lock(&ch->receiver_lock);
-    insert_into_list(&timers, timer);
-    mutex_unlock(&ch->receiver_lock);
+    mutex_lock(&timers_lock);
+    insert_into_list((struct list **) &timers, timer);
+    mutex_unlock(&timers_lock);
 }
 
 
@@ -25,10 +27,10 @@ void advance_clock(size_t ms) {
 
     for (struct timer *t = timers; t; t = t->next) {
         if (t->current < ms) {
-            fire_event(TIMER_TIMEOUT, t->ch);
+            fire_event_to(t->ch, TIMER_TIMEOUT);
 
             if (t->reset)
-                remove_from_list(&timers, t);
+                remove_from_list((struct list **) &timers, t);
 
             t->current = t->reset;
         } else {
