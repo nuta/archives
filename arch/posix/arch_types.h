81c9ff6ce80fa9ce33ee92a394a4374d0fbf0c58
Seiya Nuta <nuta@seiya.me>
2016-12-11 12:51:30 +0900

    posix: check return values from malloc(3)

--- arch/posix/arch_types.h
+++ arch/posix/arch_types.h
@@ -3,7 +3,7 @@
 
 #include "stdlibs.h"
 
-#define DEFAULT_THREAD_STACK_SIZE 8192
+#define DEFAULT_THREAD_STACK_SIZE (1024 * 16)
 #define MUTEX_INITIALIZER  PTHREAD_MUTEX_INITIALIZER
 typedef pthread_mutex_t mutex_t;
 typedef unsigned long size_t;
--- arch/posix/boot.c
+++ arch/posix/boot.c
@@ -9,7 +9,15 @@ int main(void) {
     size_t small_chunk_size = 16 * 1024;
     size_t large_chunk_size = 64 * 1024;
 
-    add_kmalloc_chunk(malloc(small_chunk_size), small_chunk_size, false);
-    add_kmalloc_chunk(malloc(large_chunk_size), large_chunk_size, true);
+    void *small_chunk = malloc(small_chunk_size);
+    void *large_chunk = malloc(large_chunk_size);
+
+    if (!small_chunk || !large_chunk) {
+        fprintf(stderr, "failed to malloc(3)\n");
+        exit(1);
+    }
+
+    add_kmalloc_chunk(small_chunk, small_chunk_size, false);
+    add_kmalloc_chunk(large_chunk, large_chunk_size, true);
     init_kernel(malloc(sizeof(struct resources)));
 }
