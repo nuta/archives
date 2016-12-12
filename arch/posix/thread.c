ab3cf213a153eed2bc85b46540282f5e42381550
Seiya Nuta <nuta@seiya.me>
2016-12-12 10:21:36 +0900

    posix: follow changes in thread interfaces

--- arch/posix/thread.c
+++ arch/posix/thread.c
@@ -4,35 +4,35 @@
 #include <kernel/panic.h>
 
 
-static tid_t current_tid; // TODO: make it a cpu-local variable
-tid_t arch_get_current_tid(void) {
+static struct thread *current_thread; // TODO: make it a cpu-local variable
+struct thread *arch_get_current_thread(void) {
 
-    return current_tid;
+    return current_thread;
 }
 
 
 NORETURN void x86_switch_thread(struct arch_thread *thread); // TODO: make it portable
-NORETURN void arch_switch_thread(tid_t tid, struct arch_thread *thread) {
+NORETURN void arch_switch_thread(struct thread *thread) {
 
-    current_tid = tid;
-    x86_switch_thread(thread);
+    current_thread = thread;
+    x86_switch_thread(&thread->arch);
     PANIC("x86_switch_thread() returned");
     for (;;); // for supress a compiler warning: 'noreturn' should not return
 }
 
 
-void arch_create_thread(struct arch_thread *thread, uintptr_t start, uintmax_t arg,
+void arch_create_thread(struct thread *thread, uintptr_t start, uintmax_t arg,
                         uintptr_t stack, size_t stack_size) {
 
 #ifdef __APPLE__
     // XXX: Without -8, a SEGV is occurred on macOS.
     //      It seems that this problem is related to the stack
     //      alignment for SSE registers.
-    thread->rsp = ((stack + stack_size) & ~0xf) - 8;
+    thread->arch.rsp = ((stack + stack_size) & ~0xf) - 8;
 #else
-    thread->rsp = (stack + stack_size) & ~0xf;
+    thread->arch.rsp = (stack + stack_size) & ~0xf;
 #endif
-    
-    thread->rip = start;
-    thread->rdi = arg;
+
+    thread->arch.rip = start;
+    thread->arch.rdi = arg;
 }
