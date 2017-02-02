87cae908c2354c051e0f7f69cd3e46908d12820a
Seiya Nuta <nuta@seiya.me>
2017-02-02 22:01:43 +0900

    cpp: don't deal with ctors/dtors

--- lang/cpp/runtime.cpp
+++ lang/cpp/runtime.cpp
@@ -1,6 +1,7 @@
 #include <runtime.h>
 #include <logging.h>
 
+#ifndef ARCH_POSIX
 
 extern int __ctors;
 extern int __ctors_end;
@@ -17,16 +18,21 @@ static void call_functions(uintptr_t start, uintptr_t end) {
     }
 }
 
+#endif // !ARCH_POSIX
 
 void call_ctors() {
 
+#ifndef ARCH_POSIX
     call_functions((uintptr_t) &__ctors, (uintptr_t)  &__ctors_end);
+#endif // !ARCH_POSIX
 }
 
 
 void call_dtors() {
 
+#ifndef ARCH_POSIX
     call_functions((uintptr_t) &__dtors, (uintptr_t)  &__dtors_end);
+#endif // !ARCH_POSIX
 }
 
 
