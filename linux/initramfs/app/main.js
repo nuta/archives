0f040550a1e3935ae814e3734904b6dd973a6ccc
Seiya Nuta <nuta@seiya.me>
2017-10-04 23:37:53 +0900

    add a startup message

--- linux/initramfs/app/main.js
+++ linux/initramfs/app/main.js
@@ -5,5 +5,6 @@ const url = process.env['MAKESTACK_SERVER_URL'];
 const deviceId = process.env['MAKESTACK_DEVICE_ID'];
 const deviceSecret = process.env['MAKESTACK_DEVICE_SECRET'];
 
+console.log("*** starting agent");
 let app = new LinuxApp(url, deviceId, deviceSecret);
 app.run();
