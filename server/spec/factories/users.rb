5754c2e635d862d14a136d46be0530254fdd29ca
Seiya Nuta <nuta@seiya.me>
2017-10-05 09:15:02 +0900

    server/spec/factories: add a workaround for too short usernames

--- server/spec/factories/users.rb
+++ server/spec/factories/users.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    username { FFaker::InternetSE.user_name.gsub(".", "-") }
+    username do
+      FFaker::InternetSE.user_name.gsub(".", "-") + 
+      Array.new(5).map{|_| Random.rand(10).to_s}.join
+    end
     email    { FFaker::Internet.email }
     password { FFaker::InternetSE.password }
     confirmed_at { Time.now }
