24516c6e4fc8dbed7ed1a66e6e0350f0f2fb22d2
Seiya Nuta <nuta@seiya.me>
2017-10-09 19:12:57 +0900

    fix a typo

--- linux/initramfs/app/main.js
+++ linux/initramfs/app/main.js
@@ -1,5 +1,5 @@
 #!/bin/node
-const AppRuntime = require('app-runime');
+const AppRuntime = require('app-runtime');
 
 const url = process.env['MAKESTACK_SERVER_URL'];
 const deviceId = process.env['MAKESTACK_DEVICE_ID'];
