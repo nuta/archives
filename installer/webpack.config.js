87512e02068f2100e9d99f62e19b59d5fdf9f517
Seiya Nuta <nuta@seiya.me>
2017-10-14 21:18:54 +0900

    installer: refactor webpack.config.js

--- installer/webpack.config.js
+++ installer/webpack.config.js
@@ -1,16 +1,28 @@
 process.env.BABEL_ENV = 'renderer'
-const production = process.env.NODE_ENV === 'production'
+const production = (process.env.NODE_ENV === 'production')
 
 const path = require('path')
 const webpack = require('webpack')
-const MinifyPlugin = require("babel-minify-webpack-plugin")
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-let rendererConfig = {
+module.exports = {
   target: 'electron-renderer',
+  devtool: production ? undefined : '#cheap-module-eval-source-map',
   entry: {
-    renderer: path.join(__dirname, 'ui/index.js')
+    renderer: path.join(__dirname, 'renderer/index.js')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      'renderer': path.join(__dirname, 'renderer'),
+      'vue$': 'vue/dist/vue.esm.js',
+      'views': path.join(__dirname, 'views')
+    },
+    extensions: ['.js', '.vue']
   },
   module: {
     rules: [
@@ -48,74 +60,21 @@ let rendererConfig = {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /\.(svg)(\?.*)?$/,
-        use: {
-          loader: 'url-loader',
-          query: {
-            limit: 10000,
-            name: 'assets/[name]--[folder].[ext]'
-          }
-        }
       }
     ]
-  },
-  node: {
-    __dirname:  !production,
-    __filename: !production
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: path.resolve(__dirname, 'ui/index.html'),
-      nodeModules: production ? false : path.resolve(__dirname, 'node_modules')
+      template: path.resolve(__dirname, 'renderer/index.html')
+    }),
+    new CopyWebpackPlugin([{
+      from: path.resolve(__dirname, 'main'),
+      to: 'dest/main'
+    }]),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': process.env.production
     }),
     new webpack.NoEmitOnErrorsPlugin()
-  ],
-  output: {
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
-    path: path.join(__dirname, 'renderer')
-  },
-  resolve: {
-    alias: {
-      'renderer': path.join(__dirname, 'ui'),
-      'vue$': 'vue/dist/vue.esm.js',
-      'views': path.join(__dirname, 'views')
-    },
-    extensions: ['.js', '.vue', '.json', '.css', '.node']
-  },
+  ]
 }
-
-switch(process.env.NODE_ENV) {
-  case 'production': 
-    rendererConfig.plugins.push(
-      new MinifyPlugin(),
-      new CopyWebpackPlugin([
-        {
-          from: path.join(__dirname, '../assets'),
-          to: path.join(__dirname, '../dist/electron/assets'),
-          ignore: ['.*']
-        }
-      ]),
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"production"'
-      }),
-      new webpack.LoaderOptionsPlugin({
-        minimize: true
-      })
-    )
-    break
-
-  case 'development':
-    rendererConfig.devtool = '#cheap-module-eval-source-map'
-    rendererConfig.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': '"development"'
-      })
-    )
-    break
-}
-
-module.exports = rendererConfig
