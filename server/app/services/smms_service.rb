2d4be2b33189eb35178b78a10d8c81e1a9d48cc6
Seiya Nuta <nuta@seiya.me>
2017-10-17 15:17:48 +0900

    server: replace logger with Rails.logger in app/services/*

--- server/app/services/smms_service.rb
+++ server/app/services/smms_service.rb
@@ -60,7 +60,7 @@ module SMMSService
     # store
     device.stores.each_with_index do |(key, store), index|
       if index >= SMMS_STORE_NUM
-        logger.warn "too many stores"
+        Rails.logger.warn "too many stores"
         break
       end
 
