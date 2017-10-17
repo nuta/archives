c59142e425a12eeca22fc4c84eab36f338c62b3b
Seiya Nuta <nuta@seiya.me>
2017-10-17 18:02:25 +0900

    server: fix call_outgoing_webhook job

--- server/app/jobs/call_outgoing_webhook_job.rb
+++ server/app/jobs/call_outgoing_webhook_job.rb
@@ -8,12 +8,13 @@ class CallOutgoingWebhookJob < ApplicationJob
     if accept_stores
       ActiveRecord::Base.transaction do
         stores = JSON.parse(resp.body).fetch("stores", {})
-        unless stores.is_a(Hash)
+        unless stores.is_a?(Hash)
           raise "`stores' received from a webhook is not Hash"
         end
         
         stores.each do |key, value|
-          store = device.device_stores.where(key: key).first_or_create
+          # TODO: accept other data types
+          store = device.device_stores.where(key: key, data_type: 'string').first_or_create
           store.value = value
           store.save!
         end
