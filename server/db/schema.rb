accca43babd78b30bbfc30d938e178a14c421ab0
Seiya Nuta <nuta@seiya.me>
2017-10-20 19:20:24 +0900

    server: add os_version to apps

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912012336) do
+ActiveRecord::Schema.define(version: 20171020055609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170912012336) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.string "api", null: false
+    t.string "os_version"
     t.index ["user_id"], name: "index_apps_on_user_id"
   end
 
