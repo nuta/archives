1cb863fef299d5072d79c92984b856b509d02f6a
Seiya Nuta <nuta@seiya.me>
2017-10-20 13:39:32 +0900

    server: fix a validation

--- server/app/models/deployment.rb
+++ server/app/models/deployment.rb
@@ -5,14 +5,18 @@ class Deployment < ApplicationRecord
   MAX_DEBUG_DATA_SIZE = 32 * 1024
   
   validates_presence_of :app
-  validates :image, length: { in: 0..MAX_IMAGE_SIZE }, allow_nil: false
-  validates :debug, length: { in: 0..MAX_DEBUG_DATA_SIZE }
+  validates :debug, length: { in: 0..MAX_DEBUG_DATA_SIZE }, allow_nil: true
   validates_uniqueness_of :version, scope: :app_id
   validates :tag, format: { with: Device::TAG_NAME_REGEX },
                   length: { in: 0..Device::TAG_LEN }, allow_nil: true
   validates :comment, length: { in: 0..1024 }, allow_nil: true
+  validate :validate_image_size
 
   before_create :set_version
+
+  def validate_image_size
+    self.image.size < MAX_IMAGE_SIZE
+  end
 
   def set_version
     self.version = (Deployment.where(app: self.app).maximum(:version) || 0) + 1
