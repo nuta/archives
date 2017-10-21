6998f37c07b68f189094d02c83faa27cc337766f
Seiya Nuta <nuta@seiya.me>
2017-10-21 16:19:08 +0900

    supervisor: suppress unzip messages

--- libs/supervisor/lib/index.js
+++ libs/supervisor/lib/index.js
@@ -57,7 +57,7 @@ class Supervisor {
 
     fs.writeFileSync(appZipPath, appZip)
     spawnSync('rm', ['-r', this.appDir])
-    spawnSync('unzip', [appZipPath, '-d', this.appDir], {
+    spawnSync('unzip', ['-q', appZipPath, '-d', this.appDir], {
       stdio: 'inherit'
     })
 
