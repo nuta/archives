13c3f9c3a5c268cccd6bade2a08ce4b682bbc668
Seiya Nuta <nuta@seiya.me>
2017-10-22 11:33:42 +0900

    server: implement throtting job rate

--- server/app/jobs/application_job.rb
+++ server/app/jobs/application_job.rb
@@ -1,2 +1,8 @@
 class ApplicationJob < ActiveJob::Base
+  include Throttle
+
+  def perform(*args)
+    raise_if_reached_limit
+    execute(*args)
+  end
 end
---
+++ server/app/jobs/concerns/throttle.rb
@@ -0,0 +1,27 @@
+module Throttle
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    attr_accessor :limit
+    attr_accessor :period
+
+    def throttle(limit:, period:)
+      self.limit = limit
+      self.period = period
+    end
+  end
+
+  def raise_if_reached_limit
+    redis_key = "job:rate:#{self.class.name.demodulize.downcase}"
+    rate = Redis.current.get(redis_key).try(:to_i) || 0
+
+    if rate >= self.class.limit
+      raise "reached the job limit: key=#{redis_key}"
+    end
+
+    Redis.current.multi do
+      Redis.current.incr(redis_key)
+      Redis.current.expire(redis_key, self.class.period.to_i)
+    end
+  end
+end
