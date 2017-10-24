f165257f6a334196bcb50bd78f28bcb132c02af8
Seiya Nuta <nuta@seiya.me>
2017-10-24 23:09:03 +0900

    server/spec: configure shoulda-matchers

--- server/spec/rails_helper.rb
+++ server/spec/rails_helper.rb
@@ -21,6 +21,13 @@ require 'webmock/rspec'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerHelpers, type: :controller
