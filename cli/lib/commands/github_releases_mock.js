e40e09a5b1b1e29e7568ee225943d0ddefc9f9fa
Seiya Nuta <nuta@seiya.me>
2017-11-05 19:44:15 +0900

    cli: fix github-releases-mock response

--- cli/lib/commands/github_releases_mock.js
+++ cli/lib/commands/github_releases_mock.js
@@ -3,7 +3,15 @@ const os = require('os')
 const path = require('path')
 const express = require('express')
 const buildPlugin = require('../plugin_builder')
-const logger = require('../logger')
+
+const DUMMY_LINUX_OS_IMAGE = `🚀🚀🚀 MOCK DEVICE OS IMAGE DATA 🛫🛫🛫'
+os_version="_____REPLACE_ME_MAKESTACK_CONFIG_OS_VERSION_____"
+device_type="_____REPLACE_ME_MAKESTACK_CONFIG_DEVICE_TYPE_____"
+device_id="_____REPLACE_ME_MAKESTACK_CONFIG_DEVICE_ID_____"
+device_secret="_____REPLACE_ME_MAKESTACK_CONFIG_DEVICE_SECRET_____"
+adapter="_____REPLACE_ME_MAKESTACK_CONFIG_ADAPTER_____"
+server_url="_____REPLACE_ME_MAKESTACK_CONFIG_SERVER_URL_abcdefghijklmnopqrstuvwxyz1234567890_____"
+`
 
 module.exports = (args, opts, logger) => {
   const server = express()
@@ -19,7 +27,7 @@ module.exports = (args, opts, logger) => {
     const images = {
       x86_64() { return fs.readFileSync('linux/build/x86_64/bootfs/kernel7.img') },
       raspberypi3() { return fs.readFileSync('linux/build/raspberrypi3/bootfs/vmlinuz') },
-      mock() { return '🚀🚀🚀 MOCK DEVICE OS IMAGE DATA 🛫🛫🛫' }
+      mock() { return DUMMY_LINUX_OS_IMAGE }
     }
 
     res.send(images[req.params.type]())
@@ -29,15 +37,18 @@ module.exports = (args, opts, logger) => {
     let json
     if (req.params.org === 'seiyanuta' && req.params.repo === 'makestack') {
       json = {
-        tag: 'v12345.0.0',
+        tag_name: 'v12345.0.0',
         assets: [{
           name: 'app-runtime.plugin.zip',
           browser_download_url: `http://localhost:8080/repos/plugins/app-runtime.plugin.zip`
+        }, {
+          name: 'makestack-linux-mock-v12345.0.0.img',
+          browser_download_url: `http://localhost:8080/repos/os/mock`
         }]
       }
     } else {
       json = {
-        tag: 'v12345.0.0',
+        tag_name: 'v12345.0.0',
         assets: [{
           name: `${req.params.repo}.plugin.zip`,
           browser_download_url: `http://localhost:8080/repos/plugins/${req.params.repo}.plugin.zip`
