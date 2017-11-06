911db7547833a349a45387438eff4951dce53dfd
Seiya Nuta <nuta@seiya.me>
2017-11-06 14:35:07 +0900

    server: fix a spec

--- server/spec/controllers/os_controller_spec.rb
+++ server/spec/controllers/os_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OsController, type: :controller do
   describe '#index' do
     it 'returns OS releases' do
       get 'index'
-      expect(JSON.parse(response.body)).to eq({ 'releases' => MakeStack.settings[:os_releases] }.to_json)
+      expect(JSON.parse(response.body)).to eq({ 'releases' => MakeStack.settings[:os_releases] })
     end
   end
 end
