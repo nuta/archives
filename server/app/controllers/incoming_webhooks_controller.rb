8b0601b683d616f31a2d78adc14ba3110575b908
Seiya Nuta <nuta@seiya.me>
2017-11-06 20:37:16 +0900

    server: fix incoming_webhooks controller

--- server/app/controllers/incoming_webhooks_controller.rb
+++ server/app/controllers/incoming_webhooks_controller.rb
@@ -5,14 +5,14 @@ class IncomingWebhooksController < ApplicationController
   before_action :set_user
 
   def invoke
-    case params[:action]
+    case params[:command]
     when 'update_app_stores'
-      update_app_stores(@app, params[:stores])
-    when 'update_devices_stores'
+      update_stores('App', @app.id, params[:stores].permit!.to_h)
+    when 'update_device_stores'
       device = @user.devices.find_by_name!(params[:device])
-      update_device_stores(device, params[:stores])
+      update_stores('Device', device.id, params[:stores].permit!.to_h)
     else
-      raise ActionController::BadRequest.new(), "unknown action"
+      raise ActionController::BadRequest.new(), "unknown command"
     end
 
     head :ok
@@ -23,7 +23,7 @@ class IncomingWebhooksController < ApplicationController
   def set_integration
     @integration = Integration.lookup_by_token(params[:token])
     unless @integration
-      head :not_found
+      head :forbidden
       return
     end
   end
@@ -36,31 +36,30 @@ class IncomingWebhooksController < ApplicationController
     @user ||= @app.user
   end
 
-  def update_app_stores(app, stores)
-    unless stores.is_a(Hash)
+  def update_stores(owner_type, owner_id, stores)
+    unless stores.is_a?(Hash)
       raise ActionController::BadRequest.new(), "`stores' must be a object'"
     end
 
     ActiveRecord::Base.transaction do
       stores.each do |key, value|
-        store = app.stores.where(key: key).first_or_create
+        store = Store.where(owner_type: owner_type, owner_id: owner_id, key: key).first_or_create
+        store.data_type = determine_data_type(value)
         store.value = value
         store.save!
       end
     end
   end
 
-  def update_device_stores(device, stores)
-    unless stores.is_a(Hash)
-      raise ActionController::BadRequest.new(), "`stores' must be a object'"
-    end
+  private
 
-    ActiveRecord::Base.transaction do
-      stores.each do |key, value|
-        store = device.device_stores.where(key: key).first_or_create
-        store.value = value
-        store.save!
-      end
+  def determine_data_type(value)
+    case value
+    when String then 'string'
+    when TrueClass, FalseClass then 'bool'
+    when Float then 'float'
+    when Integer then 'integer'
+    else raise 'unknown data type'
     end
   end
 end
