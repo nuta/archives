c7543684337ca85d489b55e43a365be05994dc97
Seiya Nuta <nuta@seiya.me>
2017-11-07 12:53:49 +0900

    server: fix seeds

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,10 +1,10 @@
 require 'ffaker'
-require 'factory_girl_rails'
+require 'factory_bot_rails'
 
 puts"Creating `test' user..."
-FactoryGirl.create(:user, username: "test", password: "12345678")
+FactoryBot.create(:user, username: "test", password: "12345678")
 
 100.times do |i|
   puts "Creating an user (#{i + 1}/100)"
-  u = FactoryGirl.create(:heavy_user)
+  u = FactoryBot.create(:heavy_user)
 end
