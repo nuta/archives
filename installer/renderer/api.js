2fdb07a90048055735a650561ad38261a82edb19
Seiya Nuta <nuta@seiya.me>
2017-11-07 21:50:05 +0900

    installer: fix credentials loader

--- installer/renderer/api.js
+++ installer/renderer/api.js
@@ -1,51 +1,37 @@
+const path = require('path')
 const fetch = require('node-fetch')
 const fs = require('fs')
 
-const CONFIG_DIR = `${process.env.HOME}/.makestack`
+const CONFIG_DIR = process.env.CONFIG_DIR || `${process.env.HOME}/.makestack`
+const CREDENTIALS_JSON = `${CONFIG_DIR}/credentials.json`
 
-class Config {
-  get server() { return this.load('server') }
-  set server(val) { return this.save('server', val) }
-  get mocks() { return this.load('mocks') }
-  set mocks(val) { return this.save('mocks', val) }
-  get credentials() { return this.load('credentials') }
-  set credentials(val) { return this.save('credentials', val) }
-
-  load(name) {
-    let json
-    try {
-      json = fs.readFileSync(`${CONFIG_DIR}/${name}.json`)
-    } catch (e) {
-      return null
-    }
-
-    return JSON.parse(json)
-  }
-
-  save(name, data) {
-    const path = `${CONFIG_DIR}/${name}.json`
-    try {
-      fs.mkdirSync(path.dirname(path))
-    } catch (e) {
-      // ignore
-    }
-
-    fs.writeFileSync(path, JSON.stringify(data))
+function loadCredentials(name) {
+  try {
+    return JSON.parse(fs.readFileSync(CREDENTIALS_JSON))
+  } catch (e) {
+    return null
   }
 }
 
-const config = new Config()
+function saveCredentials(data) {
+  fs.mkdirSync(path.dirname(CREDENTIALS_JSON))
+  fs.writeFileSync(CREDENTIALS_JSON, JSON.stringify(data))
+}
 
 module.exports = new class {
-  invoke(method, path, params, requiresCredentials = true) {
+  constructor() {
+    this.credentials = loadCredentials()
+  }
+
+  invoke(method, server, path, params, requiresCredentials = true) {
     let reqHeaders = Object.assign({
       'Content-Type': 'application/json'
-    }, config.credentials)
+    }, this.credentials)
 
     return new Promise((resolve, reject) => {
       let status
       let headers
-      fetch(`${config.server.url}/api/v1${path}`, {
+      fetch(`${this.credentials.url}/api/v1${path}`, {
         method: method,
         headers: reqHeaders,
         body: JSON.stringify(params)
@@ -65,35 +51,34 @@ module.exports = new class {
     })
   }
 
-  get serverURL() {
-    return config.server.url
-  }
-
   login(url, username, password) {
-    config.server = { url }
-    return this.invoke('POST', '/auth/sign_in', {
+    return this.invoke('POST', url, '/auth/sign_in', {
       username: username,
       password: password
     }, false).then(r => {
-      config.credentials = {
+      this.credentials = {
         username: username,
         email: r.json['data']['email'],
         uid: r.headers.get('uid'),
         'access-token': r.headers.get('access-token'),
         'access-token-secret': r.headers.get('access-token-secret')
       }
+
+      saveCredentials(this.credentials)
     })
   }
 
   isLoggedIn() {
-    return config.credentials !== null
+    return this.credentials !== null
   }
 
   updateDeviceStore(deviceName, key, value) {
-    return this.invoke('PUT', `/devices/${deviceName}/stores/${key}`, { value })
+    return this.invoke('PUT', this.credentials.url, `/devices/${deviceName}/stores/${key}`,
+      { value })
   }
 
   registerDevice(name, deviceType, tag) {
-    return this.invoke('POST', '/devices', { device: { name, device_type: deviceType, tag } })
+    return this.invoke('POST', this.credentials.url, '/devices',
+      { device: { name, device_type: deviceType, tag } })
   }
 }()
