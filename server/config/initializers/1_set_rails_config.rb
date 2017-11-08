22c46e376f63630376110f1e6603e4192c223ccf
Seiya Nuta <nuta@seiya.me>
2017-11-08 12:10:59 +0900

    server: set protocol of config.action_mailer.default_url_options

--- server/config/initializers/1_set_rails_config.rb
+++ server/config/initializers/1_set_rails_config.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {
     host: MakeStack.settings[:host],
-    port: MakeStack.settings[:port]
+    port: MakeStack.settings[:port],
+    protocol: MakeStack.settings[:protocol]
   }
 end
