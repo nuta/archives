1e0afcf70b9f57aab5bcf9fa2e81fc1e712948db
Seiya Nuta <nuta@seiya.me>
2017-11-09 10:29:55 +0900

    app-runtime: fix linux i2c api

--- libs/app-runtime/lib/linux/i2c.js
+++ libs/app-runtime/lib/linux/i2c.js
@@ -4,7 +4,9 @@ const ioctl = require('../ioctl')
 const I2C_SLAVE = 0x0703
 
 module.exports = class {
-  constructor() {
+  constructor({ address }) {
+    this.address = address
+
     if (!fs.existsSync('/dev/i2c-1')) {
       console.log('i2c device not found, disabling I2C API')
       this.fd = fs.openSync('/dev/null', 'r')
@@ -18,15 +20,15 @@ module.exports = class {
     fs.closeSync(this.fd)
   }
 
-  read(address, length) {
-    this.selectSlaveAddress(address)
+  read(length) {
+    this.selectSlaveAddress(this.address)
     let buffer = Buffer.alloc(length)
     fs.readSync(this.fd, buffer, 0, length)
     return buffer
   }
 
-  write(address, data) {
-    this.selectSlaveAddress(address)
+  write(data) {
+    this.selectSlaveAddress(this.address)
     fs.writeSync(this.fd, Buffer.from(data))
   }
 
