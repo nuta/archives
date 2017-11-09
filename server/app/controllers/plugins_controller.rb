c3d424dbe9da0856bd8c674fcc8c553f2cebf3df
Seiya Nuta <nuta@seiya.me>
2017-11-10 08:53:17 +0900

    server: support third-party plugins

--- server/app/controllers/plugins_controller.rb
+++ server/app/controllers/plugins_controller.rb
@@ -3,16 +3,17 @@ class PluginsController < ApplicationController
 
   def download
     github_repo = (official?) ? 'seiyanuta/makestack' : "#{params[:org]}/#{params[:name]}"
-    name = params[:name]
-    if Rails.env.production?
-      # TODO
-      head :not_implemented
+    plugin = params[:name]
+
+    if Rails.env.development? && official?
+      plugin_zip_path = build_plugin_locally(plugin)
+      send_file plugin_zip_path, content_type: 'application/zip'
     else
-      if name == 'nodejs-runtime'
-        # Build nodejs-runtime in the local repository.
-        plugin_zip_path = build_local_app_runtime
-        send_file plugin_zip_path, content_type: 'application/zip'
-      end
+      r = RestClient.get "https://github.com/#{github_repo}/releases/latest",
+        accept: 'application/json'
+
+      latest_tag = JSON.parse(r.body)['tag_name']
+      redirect_to "https://github.com/#{github_repo}/releases/download/#{latest_tag}/#{plugin}.#{latest_tag}.plugin.zip"
     end
   end
 
@@ -22,15 +23,25 @@ class PluginsController < ApplicationController
     (params[:org] == '_')
   end
 
-  unless Rails.env.production?
-    def build_local_app_runtime
-      plugin_zip_path = File.join(Dir.home, '.makestack', 'server', 'nodejs-runtime.plugin.zip')
+  if Rails.env.development?
+    def build_plugin_locally(plugin)
+      plugin_zip_path = File.join(Dir.home, '.makestack', 'server', "#{plugin}.plugin.zip")
       FileUtils.mkdir_p(File.dirname(plugin_zip_path))
       FileUtils.rm_f(plugin_zip_path)
 
       makestack_command = File.join(File.dirname(__FILE__), '../../../client/bin/makestack')
-      app_runtime_dir = File.join(File.dirname(__FILE__), '../../../libs/nodejs-runtime')
-      system("#{makestack_command} build-plugin #{app_runtime_dir} #{plugin_zip_path}")
+
+      if plugin == 'nodejs-runtime'
+        plugin_dir = File.join(File.dirname(__FILE__), '../../../client/nodejs-runtime')
+      else
+        plugin_dir = File.join(File.dirname(__FILE__), '../../../plugins/', plugin)
+        unless Dir.exist?(plugin_dir)
+          raise 'plugin not found'
+        end
+      end
+
+      system("#{makestack_command} build-plugin #{plugin_dir} #{plugin_zip_path}")
+
       plugin_zip_path
     end
   end
