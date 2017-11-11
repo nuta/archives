1a5e54c5846c73419f0955a687d38d2488984132
Seiya Nuta <nuta@seiya.me>
2017-11-11 16:04:05 +0900

    nodejs-runtime: add some methods to Timer API

--- client/nodejs-runtime/lib/timer.js
+++ client/nodejs-runtime/lib/timer.js
@@ -1,9 +1,32 @@
-module.exports = class {
+class TimerAPI {
   loop(interval, callback) {
-    if (interval < 0.1) {
-      interval = 0.1
-    }
-
     setInterval(callback, interval * 1000)
   }
+
+  delay(duration, callback) {
+    setTimeout(callback, duration * 1000)
+  }
+
+  sleep(duration) {
+    return new Promise((resolve, reject) => {
+      setTimeout(resolve, duration)
+    })
+  }
+
+  busywait(usec) {
+    if (Math.pow(10, 9) /* 1 sec */ <= usec) {
+      throw new Error('Too long busy wait duration.')
+    }
+
+    const start = process.hrtime()
+
+    while (true) {
+      const [secDiff, nanosecDiff] = process.hrtime(start)
+      if (secDiff > 0 || nanosecDiff > usec * 1000) {
+        return
+      }
+    }
+  }
 }
+
+module.exports = TimerAPI
