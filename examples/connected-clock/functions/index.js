1fd4f5d45be389858fb2be754f908eec74799301
Seiya Nuta <nuta@seiya.me>
2017-11-13 18:10:50 +0900

    examples/connected-clock: add weather forecast

--- examples/connected-clock/functions/index.js
+++ examples/connected-clock/functions/index.js
@@ -2,25 +2,29 @@
 
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+const fetch = require('node-fetch')
+const moment = require('moment')
+
+const OPEN_WEATHER_MAP_API_KEY = ''
+const CITY = 'Tokyo'
+const OPEN_WEATHER_MAP_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${CITY}&appid=${OPEN_WEATHER_MAP_API_KEY}`
 
 admin.initializeApp(functions.config().firebase)
 
-function padStart(str, n, fill) {
-  return fill.repeat(n - str.length) + str
-}
-
 exports.webhook = functions.https.onRequest((req, res) => {
-  const sensorData = Math.floor(req.body.body)
-  const event = req.body.event
-  const now = new Date()
-  const hours = padStart(now.getHours().toString(), 2, '0')
-  const mins = padStart(now.getMinutes().toString(), 2, '0')
+  fetch(OPEN_WEATHER_MAP_URL, { headers: { 'Content-Type': 'application/json' } })
+    .then(response => response.json()).then(json => {
+      const sensorType = req.body.event === 't' ? 'temperature' : 'humidity'
+      const sensorData = req.body.body.toFixed(2)
+      console.log(json)
+      const weather = json.list[0].weather[0].main.toUpperCase()
 
-  let messages = JSON.stringify([
-    `${hours}:${mins}`,
-    `${event === 't' ? 'temperature' : 'humidity'} ${sensorData}`,
-    `weather: i duuno`
-  ])
+      const messages = JSON.stringify([
+        moment().format('HH:mm dd  Z'),
+        `${sensorType} ${sensorData}`,
+        `weather: ${weather}`
+      ])
 
-  res.send(JSON.stringify({ stores: { messages } }))
+      res.send(JSON.stringify({ stores: { messages } }))
+    })
 })
