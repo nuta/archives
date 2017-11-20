e0f3a5a1214b54638706474e7e321e4bd806d4d7
Seiya Nuta <nuta@seiya.me>
2017-11-20 12:10:14 +0900

    remove tsc artifacts

--- client/supervisor/dist/devices/x86_64.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-module.exports = class {
-    updateOS(imagePath) {
-        fs.renameSync(imagePath, '/boot/vmlinuz');
-    }
-};
