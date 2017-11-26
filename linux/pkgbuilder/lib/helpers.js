5f68a3b830da9bfa2e74da90281a90b6facbfca8
Seiya Nuta <nuta@seiya.me>
2017-11-26 22:47:02 +0900

    linux: refactor pkgbuilder

--- linux/pkgbuilder/lib/helpers.js
+++ linux/pkgbuilder/lib/helpers.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { spawnSync } = require('child_process')
 
 function find(basedir) {
   let files = []
@@ -64,4 +65,46 @@ function copyFiles(srcDir, destDir, ignorePatterns = []) {
   }
 }
 
-module.exports = { find, mkdirp, copyFiles }
+function shasum(filepath) {
+  const cp = spawnSync('shasum', ['-a', '256', filepath], {
+    stdio: 'pipe',
+    encoding: 'utf-8'
+  })
+
+  if (cp.error)
+    throw new Error(cp.error)
+
+  return cp.stdout.split(' ')[0]
+}
+
+function download(url, sha256) {
+  const filepath = path.resolve(build.downloadsDir, path.basename(url))
+  if (fs.existsSync(filepath)) {
+    // TODO: compare SHASUM
+    return filepath
+  }
+
+  spawnSync('curl', ['-fSLo', filepath, url], { stdio: 'inherit' })
+  return filepath
+}
+
+function extract(filepath, dest) {
+  if (fs.existsSync(dest))
+    return
+
+  if (filepath.match(/\.tar\.(xz|bz2?|gz)$/)) {
+    mkdirp(dest)
+    spawnSync('tar', ['xf', filepath, '-C', dest, '--strip-components', '1'], { stdio: 'inherit' })
+  } else if (filepath.match(/\.deb$/)) {
+    mkdirp(dest)
+    const cwd = process.cwd()
+    process.chdir(dest)
+    spawnSync('ar', ['x', filepath], { stdio: 'inherit' })
+    spawnSync('tar', ['vxf', 'data.tar.xz'], { stdio: 'inherit' })
+    process.chdir(cwd)
+  } else {
+    throw new Error(`unsupported file type: ${filepath}`)
+  }
+}
+
+module.exports = { find, mkdirp, copyFiles, shasum, download, extract }
