336b06a9f9d78517822024f9022a52cd563f098f
Seiya Nuta <nuta@seiya.me>
2017-12-01 08:49:39 +0900

    runtime: introduce TypeScript

--- runtime/lib/logger.ts
+++ runtime/lib/logger.ts
@@ -1,6 +1,6 @@
-const util = require('util')
+import * as util from 'util';
 
-function print(level, ...messages) {
+function log(level: 'DEBUG' |  'INFO' | 'ERROR' | 'WARN', ...messages: string[]) {
   const colors = {
     'DEBUG': '35',
     'INFO': '34',
@@ -25,25 +25,18 @@ function print(level, ...messages) {
   console.log(`[\x1b[1;32mruntime\x1b[0m ${time} \x1b[${color}m${level}\x1b[0m] ${body}`)
 }
 
-function debug(...messages) {
-  print('DEBUG', ...messages)
+export function debug(...messages) {
+  log('DEBUG', ...messages)
 }
 
-function info(...messages) {
-  print('INFO', ...messages)
+export function info(...messages) {
+  log('INFO', ...messages)
 }
 
-function error(...messages) {
-  print('ERROR', ...messages)
+export function error(...messages) {
+  log('ERROR', ...messages)
 }
 
-function warn(...messages) {
-  print('WARN', ...messages)
-}
-
-module.exports = {
-  debug,
-  info,
-  error,
-  warn
+export function warn(...messages) {
+  log('WARN', ...messages)
 }
