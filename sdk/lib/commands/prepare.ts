de12b030f78d499f81d509f046621feac3f51baa
Seiya Nuta <nuta@seiya.me>
2017-12-01 10:28:06 +0900

    sdk/prepare: download plugins

--- sdk/lib/commands/prepare.ts
+++ sdk/lib/commands/prepare.ts
@@ -1,25 +1,59 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { spawnSync } from 'child_process';
+import * as JSZip from 'jszip';
+import * as yaml from 'js-yaml';
+import { api } from '../api';
 import { createFile } from '../helpers';
 import { logger } from '../logger';
 
-export function main(args, opts) {
-  const makestackTypeFile = path.resolve(__dirname, '../../makestack.d.ts')
+function installDevDependencies() {
+  const { status, stdout, stderr } = spawnSync('yarn', [], { encoding: 'utf-8' })
+  if (status !== 0) {
+    throw new Error(`yarn exited with ${status}:\n${stdout}\n${stderr}`)
+  }
+}
 
-  if (!fs.existsSync('app.yaml') && !fs.existsSync('plugin.yaml')) {
+async function downloadAndExtractPlugin(plugin: string) {
+  const pluginZip = await (new JSZip()).loadAsync(await api.downloadPlugin(plugin))
+
+  for (const filepath in pluginZip.files) {
+    if (!filepath.endsWith('/')) {
+      console.log(filepath)
+      const body = pluginZip.files[filepath].async('arraybuffer')
+      createFile(path.join('node_modules/@makestack', plugin, filepath), body)
+    }
+  }
+}
+
+export async function main(args, opts) {
+  const makestackTypeFile = path.resolve(__dirname, '../../runtime/index.d.ts')
+
+  let yamlPath
+  if (fs.existsSync('app.yaml')) {
+    yamlPath = 'app.yaml'
+  } else if (fs.existsSync('plugin.yaml')) {
+    yamlPath = 'plugin.yaml'
+  } else {
     throw new Error('The current directory is not an app nor a plugin.')
   }
 
   if (fs.existsSync('package.json')) {
     logger.info('Installing npm dev dependecies')
-    const { status, stdout, stderr } = spawnSync('yarn', [], { encoding: 'utf-8' })
-    if (status !== 0) {
-      throw new Error(`yarn exited with ${status}:\n${stdout}\n${stderr}`)
+    installDevDependencies()
+  }
+
+  const { plugins } = yaml.safeLoad(fs.readFileSync(yamlPath))
+  for (const plugin of plugins || []) {
+    if (fs.existsSync(path.join('node_modules/@makestack', plugin))) {
+      logger.progress(`Download @makestack/${plugin}`)
+      downloadAndExtractPlugin(plugin)
+    } else {
+      logger.progress(`Download @makestack/${plugin} (alredy exists)`)
     }
   }
 
-  logger.info('Add @types/makestack')
+  logger.progress('Add @types/makestack')
   createFile('node_modules/@types/makestack/index.d.ts',
       fs.readFileSync(makestackTypeFile))
 }
