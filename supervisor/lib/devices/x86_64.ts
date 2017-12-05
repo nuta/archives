e06f7df451f0a3edbc8593e298cc02ca6da2b9b9
Seiya Nuta <nuta@seiya.me>
2017-12-05 14:53:40 +0900

    supervisor: fix x64 updateOS

--- supervisor/lib/devices/x86_64.ts
+++ supervisor/lib/devices/x86_64.ts
@@ -3,6 +3,6 @@ import { DeviceBase } from "./device_base";
 
 export class Device extends DeviceBase {
     public updateOS(image: Buffer) {
-        fs.writeFileSync("/boot/vmlinuz", image);
+        fs.writeFileSync("/EFI/boot/bootx64.efi", image);
     }
 }
