4aca0d409c54dea40bc83da4da358a744904ff6f
Seiya Nuta <nuta@seiya.me>
2017-12-05 09:39:52 +0900

    runtime: fix App api

--- runtime/lib/api/app.ts
+++ runtime/lib/api/app.ts
@@ -1,11 +1,11 @@
 export class AppAPI {
-  public enableUpdate() {
-    process.send({ type: "setUpdateLock", body: 'lock' });
-  }
+    public enableUpdate() {
+        process.send({ type: "setUpdateLock", body: 'unlock' });
+    }
 
-  public disableUpdate() {
-    process.send({ type: "setUpdateLock", body: 'unlock' });
-  }
+    public disableUpdate() {
+        process.send({ type: "setUpdateLock", body: 'lock' });
+    }
 
     public onExit(callback) {
         process.on("SIGTERM", () => {
