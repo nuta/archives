dcb4b0f2967ffd9a36070ae07cbe2b657156d225
Seiya Nuta <nuta@seiya.me>
2017-12-08 05:45:30 +0900

    server: combine os_releases.*.yml

--- server/config/initializers/0_settings.rb
+++ server/config/initializers/0_settings.rb
@@ -1,16 +1,21 @@
 module MakeStack
   def self.settings
-    @settings ||= load_config('settings')
+    @settings ||= load_config('settings', true)
   end
 
   def self.os_releases
-    @os_releases ||= load_config('os_releases')
+    @os_releases ||= load_config('os_releases', false)
   end
 
   private
 
-  def self.load_config(name)
-    suffix = (Rails.env.production?) ? '.production' : ".development"
+  def self.load_config(name, add_env_suffix)
+    if add_env_suffix
+      suffix = (Rails.env.production?) ? '.production' : ".development"
+    else
+      suffix = ''
+    end
+
     filename = "#{name}#{suffix}.yml"
     YAML.load(open("#{Rails.root}/config/#{filename}").read).with_indifferent_access
   end
