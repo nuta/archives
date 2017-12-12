1520bf762186b0f399960efffbe8766d929b5932
Seiya Nuta <nuta@seiya.me>
2017-12-12 10:30:56 +0900

    sdk/plugin_builder: print progress

--- sdk/lib/plugin_builder.ts
+++ sdk/lib/plugin_builder.ts
@@ -3,13 +3,20 @@ import * as fs from "fs";
 import * as os from "os";
 import * as path from "path";
 import { FatalError } from "./types";
+import { logger } from "./logger";
 
 function buildDockerImage() {
-    const { status } = spawnSync("docker", ["build", "-t", "makestack/plugin-builder", "."], {
+    logger.progress('building a Docker image');
+
+    const { error, status } = spawnSync("dockers", ["build", "-t", "makestack/plugin-builder", "."], {
         stdio: "inherit",
         env: process.env, // DOCKER_HOST, etc.
         cwd: path.resolve(__dirname, "../plugin_builder"),
     });
+
+    if ((error as any).code === 'ENOENT') {
+        throw new FatalError("`docker' command is not installed.");
+    }
 
     if (status !== 0) {
         throw new FatalError(`docker build exited with ${status}`)
@@ -17,13 +24,16 @@ function buildDockerImage() {
 }
 
 function runContainer(pluginDir: string) {
+    logger.progress('building the plugin');
     const tempdir = path.join(os.homedir(), ".makestack", "plugin-builder");
 
     // Note that docker-machine in macOS does not support mounting volumes
     // outside the home directory.
     const { status } = spawnSync("docker",
-        [ "run", "--rm", "-v", `${path.resolve(pluginDir)}:/plugin:ro`,
-          "-v", `${tempdir}:/dist`, "-t", "makestack/plugin-builder" ], {
+        [
+            "run", "--rm", "-v", `${path.resolve(pluginDir)}:/plugin:ro`,
+            "-v", `${tempdir}:/dist`, "-t", "makestack/plugin-builder"
+        ], {
         stdio: "inherit",
         env: process.env, // DOCKER_HOST, etc.
     });
