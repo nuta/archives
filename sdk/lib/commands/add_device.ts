3634bb4edc833eea5cf590ab4b95ad50af5dc1be
Seiya Nuta <nuta@seiya.me>
2018-02-11 22:02:33 +0900

    sdk: fix add-device command

--- sdk/lib/commands/add_device.ts
+++ sdk/lib/commands/add_device.ts
@@ -2,9 +2,7 @@ import { api } from "../api";
 import { loadPackageJson } from "../appdir";
 
 export async function main(args: any, opts: any, logger: any) {
-    const appName = loadPackageJson(opts.appDir).name;
-
-    await api.updateDevice(args.deviceName, {
-        app: appName,
+    await api.updateDevice(args.name, {
+        app: opts.app
     });
 }
