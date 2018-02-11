5c9f470d79972efd1d5709d5ac3112046bd5d248
Seiya Nuta <nuta@seiya.me>
2018-02-11 21:50:20 +0900

    ui: add guide-box

---
+++ ui/components/guide-box.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="guide-box">
+    <section>
+      <slot></slot>
+    </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.guide-box {
+  text-align: center;
+
+  h1 {
+    font-size: 25px;
+  }
+}
+</style>
