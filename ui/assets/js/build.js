609c446d9d2969b8f3d8e398ff01a732b8781f30
Seiya Nuta <nuta@seiya.me>
2018-02-15 09:57:06 +0900

    ui/build: bundle start.js

--- ui/assets/js/build.js
+++ ui/assets/js/build.js
@@ -1,10 +1,26 @@
 const JSZip = require('jszip')
 
+const startJs = `
+function __handleError(error) {
+  console.error(error.stack)
+  process.send({
+    type: 'log',
+    body: '!' + error.stack.replace(/\\n/g, "\\n\\\\")
+  })
+  process.exit(1)
+}
+
+process.on('unhandledRejection', __handleError)
+process.on('uncaughtException', __handleError)
+require('./app')
+`
+
 export async function buildApp(code) {
   let zip = new JSZip()
 
   // Copy app files.
   zip.file('app.js', code)
+  zip.file('start.js', startJs)
 
   const zipImage = await zip.generateAsync({
     type: 'arraybuffer',
