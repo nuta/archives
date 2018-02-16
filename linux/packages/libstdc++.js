2f4d74d0de8bc07e086b94280f870e6a1436e9b5
Seiya Nuta <nuta@seiya.me>
2018-02-16 09:28:11 +0900

    linux: fix libstdc++ build

--- linux/packages/libstdc++.js
+++ linux/packages/libstdc++.js
@@ -1,6 +1,6 @@
 const { config } = require('../pkgbuilder').pkg
 
-const version = '7.2.0-18ubuntu2'
+const version = '7.2.0-8ubuntu3'
 
 module.exports = {
   name: 'libstdc++',
@@ -13,8 +13,8 @@ module.exports = {
 
   sha256() {
     switch (config('target.deb_arch')) {
-      case 'amd64': return 'dea90151dd5ac0626cde33ab455cfcac553ca3fe4a4438cfba5c02308bde359a'
-      case 'armhf': return '2204858ca2bee2788162d80fa130b6443d15b29f13543b48636f1bf802190c01'
+      case 'amd64': return '156fc7ca6bf91053b7dde818fa0e5ec1b49b725ff5922b19b44f8c90fc1d3e70'
+      case 'armhf': return '9147db55ec6c9f97e95131cd5cf804a4c0b4f3bfbf18d65a392ebef7d3b020f4'
       default: throw new Error(`unknown target.deb_arch: \`${config('deb_arch')}'`)
     }
   },
