5c8393a44f1cc7ef74666e3828bbd96d906832ec
Seiya Nuta <nuta@seiya.me>
2018-04-04 15:36:18 +0900

    ui/build: minify code

--- ui/assets/js/build.js
+++ ui/assets/js/build.js
@@ -2,6 +2,7 @@ export async function buildApp(appCode) {
   // App image is either zip or plain JavaScript source code.
   const { code, map } = Babel.transform(appCode, {
     sourceMaps: true,
+    minified: true,
     presets: [
       ['es2015', { modules: false }]
     ]
