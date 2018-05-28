f1c857f2b4c3584dd8e7ec4771fccbf7b00c2749
Seiya Nuta <nuta@seiya.me>
2018-05-28 14:46:39 +0900

    rust: fix print!

--- libs/rust/src/print.rs
+++ libs/rust/src/print.rs
@@ -1,8 +1,7 @@
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => {
-        use resea::interfaces::logging::{Logging};
-        Logging::from_cid(1).emit(format!($($arg)*).as_bytes()).ok();
+        resea::interfaces::logging::Logging::from_cid(1).emit(format!($($arg)*).as_bytes()).ok();
     };
 }
 
