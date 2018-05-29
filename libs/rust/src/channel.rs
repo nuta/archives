29d780b3a74538a9a7c70d58dd5db968c6b976e5
Seiya Nuta <nuta@seiya.me>
2018-05-29 10:25:01 +0900

    rust: implement Channel::from_cid

--- libs/rust/src/channel.rs
+++ libs/rust/src/channel.rs
@@ -5,6 +5,10 @@ pub struct Channel {
 }
 
 impl Channel {
+    pub fn from_cid(cid: CId) -> Channel {
+        Channel { cid: cid }
+    }
+
     pub fn to_cid(&self) -> CId {
         self.cid
     }
