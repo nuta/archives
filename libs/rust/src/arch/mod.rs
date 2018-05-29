048736c396833911a6e4ab523ac0ccb051c52d49
Seiya Nuta <nuta@seiya.me>
2018-05-29 10:24:40 +0900

    rust: convert CId to i64

--- libs/rust/src/arch/mod.rs
+++ libs/rust/src/arch/mod.rs
@@ -3,7 +3,7 @@ global_asm!(include_str!("../../../resea/arch/x64/syscall.S"));
 #[cfg(target_arch="x86_64")]
 pub mod x64;
 
-pub type CId = i32;
+pub type CId = i64;
 pub type Header = u64;
 pub type Payload = u64;
 
