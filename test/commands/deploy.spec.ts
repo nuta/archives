943e0917ddd82ae0956ea2a73cb3c9ee8c464413
Seiya Nuta <nuta@seiya.me>
2018-06-11 15:27:31 +0900

    add a test

---
+++ test/commands/deploy.spec.ts
@@ -0,0 +1,34 @@
+import "mocha";
+import * as sinon from "sinon";
+import * as path from "path";
+import * as fs from "fs-extra";
+import * as childProcess from "child_process";
+import { expect } from "chai";
+import { main } from "../..";
+
+describe("deploy command", function() {
+    beforeEach(async function() {
+        const spawnSync = childProcess.spawnSync;
+        sinon.stub(childProcess, "spawnSync")
+            .callsFake(() =>
+                spawnSync("echo", ["-n"])
+            );
+
+        this.cwd = process.cwd()
+        this.appDir = "test-app"
+        fs.removeSync(this.appDir)
+        await main.run(['new', this.appDir])
+    })
+
+    afterEach(function() {
+        fs.removeSync(this.appDir)
+    })
+
+    it("deploys successfully", async function() {
+        await main.run([
+            'deploy', '--app-dir', this.appDir,
+            '--firebase-project', 'makestack-hello-world',
+            '--platform', 'firebase'
+        ]);
+    })
+})
