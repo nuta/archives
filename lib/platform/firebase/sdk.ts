e4c16179cf48dba4bcc437f96840b40568024bea
Seiya Nuta <nuta@seiya.me>
2018-06-13 18:55:16 +0900

    firebase: refactor deploy

--- lib/platform/firebase/sdk.ts
+++ lib/platform/firebase/sdk.ts
@@ -4,14 +4,46 @@ import * as EventEmitter from "events";
 import * as readline from "readline";
 import { PlatformSdk } from "../sdk";
 import { DeployOptions } from "../../types";
-import { execWithPipe, exec } from "../../helpers";
+import { execCmdWithPipe, execCmd } from "../../helpers";
 import { logger } from "../../logger";
 const { blue } = require("chalk");
+
+const FIREBASE_JSON_TMPL = JSON.stringify({
+    hosting: {
+        public: "public",
+        ignore: [
+            "**/.*",
+        ],
+        rewrites: [
+            {
+                source: "/makestack/**",
+                function: "makestack",
+            },
+        ],
+    },
+});
 
 export class FirebasePlatformSdk extends PlatformSdk {
     async command(deviceName: string, command: string, arg: string): Promise<any> {
         // TODO
         return {};
+    }
+
+    private preparePackageJsonForDeploy(packageJsonPath: string, projDir: string, appDir: string) {
+        fs.writeJsonSync(path.join(projDir, "firebase.json"), FIREBASE_JSON_TMPL);
+        const { dependencies } = fs.readJsonSync(packageJsonPath);
+        const blacklist = ["serialport"];
+        for (const dep of blacklist) {
+            delete dependencies[dep];
+        }
+
+        const appPackageJson = fs.readJsonSync(path.resolve(appDir, "package.json"));
+        return {
+            private: true,
+            main: "./index.js",
+            dependencies: Object.assign({}, dependencies, appPackageJson.dependencies),
+            makestack: appPackageJson.makestack,
+        };
     }
 
     async deploy(appDir: string, opts: DeployOptions) {
@@ -21,82 +53,32 @@ export class FirebasePlatformSdk extends PlatformSdk {
 
         const projDir = path.join(appDir, "build/deploy");
         const pkgDir = path.join(appDir, "build/deploy/functions");
+        const pluginsDir = path.resolve(__dirname, "../../../../plugins");
+        const packageJsonPath = path.resolve(__dirname, "../../../../package.json");
+        const packageJson = this.preparePackageJsonForDeploy(packageJsonPath, projDir, appDir);
 
         fs.mkdirpSync(pkgDir);
-
-        fs.writeJsonSync(path.join(projDir, "firebase.json"), {
-            hosting: {
-                public: "public",
-                ignore: [
-                    "**/.*",
-                ],
-                rewrites: [
-                    {
-                        source: "/makestack/**",
-                        function: "makestack",
-                    },
-                ],
-            },
-        });
-
-        const packageJsonPath = path.resolve(__dirname, "../../../../package.json");
-        const pluginsDir = path.resolve(__dirname, "../../../../plugins");
-        const { dependencies } = fs.readJsonSync(packageJsonPath);
-        const blacklist = ["serialport"];
-        for (const dep of blacklist) {
-            delete dependencies[dep];
-        }
-
-        const appPackageJson = fs.readJsonSync(path.resolve(appDir, "package.json"));
-        fs.writeJsonSync(path.join(pkgDir, "package.json"), {
-            private: true,
-            main: "./index.js",
-            dependencies: Object.assign({}, dependencies, appPackageJson.dependencies),
-            makestack: appPackageJson.makestack,
-        });
-
+        fs.writeJsonSync(path.join(pkgDir, "package.json"), packageJson);
         fs.mkdirpSync(path.join(pkgDir, "makestack"));
-        fs.copyFileSync(
-            packageJsonPath,
-            path.join(pkgDir, "makestack/package.json"),
-        );
-
-        fs.copySync(
-            path.join(pluginsDir),
-            path.join(pkgDir, "makestack/plugins"),
-        );
-
-        fs.copySync(
-            path.join(appDir, "public"),
-            path.join(projDir, "public"),
-        );
-
-        fs.copyFileSync(
-            path.join(appDir, "server.js"),
-            path.join(pkgDir, "server.js"),
-        );
-
-        fs.copyFileSync(
-            path.join(appDir, "esp32.firmware"),
-            path.join(pkgDir, "esp32.firmware"),
-        );
-
-        fs.copyFileSync(
-            path.resolve(__dirname, "start.js"),
-            path.join(pkgDir, "index.js"),
-        );
+        fs.copySync(packageJsonPath, path.join(pkgDir, "makestack/package.json"));
+        fs.copySync(path.join(pluginsDir), path.join(pkgDir, "makestack/plugins"));
+        fs.copySync(path.join(appDir, "public"), path.join(projDir, "public"));
+        fs.copySync(path.join(appDir, "server.js"), path.join(pkgDir, "server.js"));
+        fs.copySync(path.join(appDir, "esp32.firmware"), path.join(pkgDir, "esp32.firmware"));
+        fs.copySync(path.resolve(__dirname, "start.js"), path.join(pkgDir, "index.js"));
 
         logger.progress("Installing dependencies");
-        exec(["yarn"], { cwd: pkgDir });
+        execCmd(["yarn"], { cwd: pkgDir });
 
         logger.progress("Transpiling");
-        exec(["yarn", "run", "babel", "--presets=es2015,stage-3",
+        execCmd([
+            "yarn", "run", "babel", "--presets=es2015,stage-3",
             "--out-dir", path.join(pkgDir, "makestack/dist"),
             path.resolve(__dirname, "../../../../dist"),
         ], { cwd: pkgDir });
 
         logger.progress("firebase deploy (it may takes long)");
-        exec(["firebase", "deploy", "--project", opts.firebaseProject], { cwd: projDir });
+        execCmd(["firebase", "deploy", "--project", opts.firebaseProject], { cwd: projDir });
     }
 
     public viewLog(opts: any) {
@@ -106,7 +88,7 @@ export class FirebasePlatformSdk extends PlatformSdk {
 
         const readLog = () => {
             const argv = ["firebase", "functions:log", "--project", opts.firebaseProject];
-            const { stdout } = execWithPipe(argv);
+            const { stdout } = execCmdWithPipe(argv);
             lastLine = stdout.split("\n").filter((l) => l.length > 0).pop();
             return stdout;
         };
