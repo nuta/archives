b3e3095725c0de5737281e38ec02e5885aed82ff
Seiya Nuta <nuta@seiya.me>
2018-07-04 14:04:21 +0900

    rust: declare some system calls

--- libs/rust/src/arch/mod.rs
+++ libs/rust/src/arch/mod.rs
@@ -14,14 +14,11 @@ extern "C" {
     pub fn ipc_call(ch: CId, header: Header, a0: Payload, a1: Payload,
                     a2: Payload, a3: Payload, r0: *mut Payload, r1: *mut Payload,
                     r2: *mut Payload, r3: *mut Payload) -> Header;
-
-    /*
-    pub fn Header ipc_send(ch: Cid, Header type, a0: Payload, a1: Payload,
-                           a2: Payload, a3: Payload);
-    pub fn Header ipc_recv(ch: Cid, CId *mut from, *mut a0: Payload, *mut a1: Payload,
-                           *mut a2: Payload, *mut a3: Payload);
-    pub fn Header ipc_replyrecv(CId client, Header type, r0: Payload, r1: Payload,
-                                r2: Payload, r3: Payload, *mut a0: Payload, *mut a1: Payload,
-                                *mut a2: Payload, *mut a3: Payload);
-    */
+    pub fn ipc_send(ch: CId, header: Header, a0: Payload, a1: Payload,
+                    a2: Payload, a3: Payload) -> Header;
+    pub fn ipc_recv(ch: CId, from: *mut CId, a0: *mut Payload, a1: *mut Payload,
+                    a2: *mut Payload, a3: *mut Payload) -> Header;
+    pub fn ipc_replyrecv(server: CId, client: *mut CId, header: Header, r0: Payload, r1: Payload,
+                         r2: Payload, r3: Payload, a0: *mut Payload, a1: *mut Payload,
+                         a2: *mut Payload, a3: *mut Payload) -> Header;
 }
