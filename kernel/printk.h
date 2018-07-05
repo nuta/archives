8e5780a33cbc33db1d2af53816fae128922a2214
Seiya Nuta <nuta@seiya.me>
2018-07-05 12:00:50 +0900

    add WARN

--- kernel/printk.h
+++ kernel/printk.h
@@ -5,6 +5,7 @@ void printk(const char *fmt, ...);
 
 #define DEBUG(fmt, ...) printk(fmt "\n", ## __VA_ARGS__)
 #define INFO(fmt, ...) printk(fmt "\n", ## __VA_ARGS__)
+#define WARN(fmt, ...) printk("WARN: " fmt "\n", ## __VA_ARGS__)
 #define BUG(fmt, ...) printk("BUG: " fmt "\n", ## __VA_ARGS__)
 #define PANIC(fmt, ...) do { \
         printk("PANIC: " fmt "\n", ## __VA_ARGS__); \
