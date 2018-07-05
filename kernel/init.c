6a12365993b347c24ef4ac7661f92b9b5ae9beef
Seiya Nuta <nuta@seiya.me>
2018-07-05 12:01:39 +0900

    remove a PANIC

--- kernel/init.c
+++ kernel/init.c
@@ -142,7 +142,6 @@ void kernel_init(void) {
     // From here, we're in the idle thread context. The thread
     // is resumed when there are no other threads to run.
     for (;;) {
-        PANIC("idling...\n");
         // Sleep until an interrupt occurs in order not to heat up the computer
         // and to save money on electricity.
         arch_idle();
