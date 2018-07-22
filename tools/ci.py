f28708cb4e946114882199bb409ce28e84757ece
Seiya Nuta <nuta@seiya.me>
2018-07-22 17:14:33 +0900

    ci: refactor scripts

---
+++ tools/ci.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+
+antlr = "java -jar {}/antlr-4.7.1-complete.jar".format(os.getcwd())
+
+def test_kernel(arch):
+    subprocess.run(
+        [
+            "make", "-j2", "build",
+            "ARCH=" + arch,
+            "ANTLR4=" + antlr,
+            "SERVERS=test",
+            "CC=clang-6.0",
+            "LD=ld.lld-6.0"
+        ],
+        check=True
+    )
+
+def test_lib(lib):
+    subprocess.run(["cargo", "test"], cwd=os.path.join("libs", lib), check=True)
+
+def main():
+    subprocess.run(["make", "stubs", "ANTLR4=" + antlr], check=True)
+
+    if os.environ["M"] == "kernel":
+        test_kernel(os.environ["ARCH"])
+    elif os.environ["M"] == "lib":
+        test_lib(os.environ["LIB"])
+
+if __name__ == "__main__":
+    main()
