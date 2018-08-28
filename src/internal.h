51d8f789cad08fa64e9222b2b614f561ab8c35b0
Seiya Nuta <nuta@seiya.me>
2018-08-28 13:22:20 +0900

    use _setjmp and _longjmp
    Avoid storing/restoring the signal mask to improve performance.

--- src/internal.h
+++ src/internal.h
@@ -22,8 +22,8 @@
 #define ena_memchr memchr
 #define ena_snprintf snprintf
 #define ENA_ASSERT assert
-#define ena_setjmp setjmp
-#define ena_longjmp longjmp
+#define ena_setjmp _setjmp
+#define ena_longjmp _longjmp
 #define ena_jmpbuf jmp_buf
 
 #ifdef ENA_DEBUG_BUILD
