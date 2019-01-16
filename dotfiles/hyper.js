f40272bb14acd541248d2ab039b294fb60e325bc
Seiya Nuta <nuta@seiya.me>
2019-01-16 09:43:08 +0900

    add old dotfiles

---
+++ dotfiles/hyper.js
@@ -0,0 +1,46 @@
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 12,
+    fontFamily: '"Source Code Pro", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorShape: 'BEAM',
+    cursorBlink: false,
+    css: '',
+    termCSS: '',
+    showHamburgerMenu: '',
+    showWindowControls: '',
+    padding: '12px 14px',
+
+
+    colors: {
+      black: '#000000',
+      red: '#ff0000',
+      green: '#33ff00',
+      yellow: '#ffff00',
+      blue: '#0066ff',
+      magenta: '#cc00ff',
+      cyan: '#00ffff',
+      white: '#d0d0d0',
+      lightBlack: '#808080',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#ffffff'
+    },
+
+    shell: '',
+    shellArgs: ['--login'],
+    env: {},
+    bell: 'SOUND',
+    copyOnSelect: false,
+    quickEdit: true
+  },
+  plugins: [],
+  localPlugins: [],
+  keymaps: {
+  }
+}
